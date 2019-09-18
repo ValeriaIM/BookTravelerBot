1c9e73d270ee88cf8d48c0a3c9910b9a65f5ad68
KIVIrina <50109796+KIVIrina@users.noreply.github.com>
2019-09-19 00:53:52 +0500

    Update Main.java
    нужно добавить этот код, чтобы можно было вызывать время

--- Main.java
+++ Main.java
@@ -49,3 +49,18 @@ public class Main {
         }
     }
 }
+
+/*
+import java.util.Date;
+
+public class Test {
+
+   public static void main(String args[]) {
+      // Инициализация объекта date
+      Date date = new Date();
+
+      // Вывод текущей даты и времени с использованием toString()
+      System.out.println(date.toString());
+   }
+} 
+*/
