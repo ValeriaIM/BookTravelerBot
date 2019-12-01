9e53e3ac4d2ebed05693d7ce58e8ec0af28e461c
ValeriaIM <50073123+ValeriaIM@users.noreply.github.com>
2019-12-01 17:29:50 +0500

    Потокобезопасный бот
    я вынесла все "текущие" состояния в отдельный класс( позже скину) + hashmap <Пользователь, его данные> = решение проблемы с потокобезопасностью. Также вынесла часть методов самого бота в бот примитив, часть в ридер - он работает с файлами.

--- Bot.java
+++ Bot.java
@@ -1,54 +1,33 @@
 import org.o7planning.googledrive.quickstart.GoogleDrive;
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.text.DateFormat;
 import java.util.*;
 
-interface MyFunc{
-    void func(Message message);
-}
-public class Bot extends TelegramLongPollingBot {
-    public Boolean flChoose = false;
-    public Boolean flEcho = false;
-
-    enum State{
-        Main,
-        Library,
-        Read
-    }
-
-    private State botState = State.Main;
-    public HashMap<String, MyFunc> currentCommands = createPrimitiveCommands();
+public class Bot extends BotPrimitive {
+    private HashMap<String, UserDates> Users = new HashMap<>();
+    private Reader reader = new Reader();
     private GoogleDrive googleDrive;
+
     {
         try {
             googleDrive = new GoogleDrive();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (GeneralSecurityException e) {
+        } catch (IOException | GeneralSecurityException e) {
             e.printStackTrace();
         }
     }
 
-    private int countBookInLibrary = 2;
-    private int currentBook = 0;
-    private ArrayList<String> currentParagraphsList = new ArrayList<>();
-    private int currentPosition = 0;
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotApi = new TelegramBotsApi();
         try {
@@ -61,128 +40,125 @@ public class Bot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) { //прием сообщений, получение обновлений, реализованный на лонгпул - запрос
         Message message = update.getMessage();
-
-        if (message != null && message.hasText()){
+        if (message != null && message.hasText()) {
             processingMessage(message);
         }
     }
 
-    public void processingMessage(Message message) throws IOException {
-        if (flEcho) {
+    private void processingMessage(Message message){
+        var chatId = message.getChatId().toString();
+        if (!Users.containsKey(chatId)) {
+            addNewUser(chatId);
+        }
+        var currentCommands = getUserCommands(message);
+        var userDates = getUserDates(message);
+        if (userDates.getFlEcho()) {
             echo(message);
-            flEcho = false;
-        }
-        else if (flChoose) {
+            userDates.setFlEcho(false);
+        } else if (userDates.getFlChoose()) {
             chooseBook(message);
-            flChoose = false;
-        }
-        else if (currentCommands.containsKey(message.getText())){
+            userDates.setFlChoose(false);
+        } else if (currentCommands.containsKey(message.getText())) {
             MyFunc func = currentCommands.get(message.getText());
             func.func(message);
-        }
-        else
+        } else
             sendMsg(message, "Неверная команда, попробуй еще раз.");
     }
 
-    public void sendMsg(Message message, String text) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.enableMarkdown(true);
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText(text);
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = getSendMessage(message, text);
         try {
-            addButtons(sendMessage);
+            addButtons(sendMessage, message);
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
 
-    public String getBotUsername() { // Вернуть имя бота при регистрации
-        return "BookTraveler";
-    }
-
-    public String getBotToken() { // Вернуть полученный токен
-        return "741739778:AAFfKTQkbLQkePnWPJaRhe11uAJFnUYcfaM";
-    }
-
-    public void addButtons(SendMessage sendMessage) {
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();//инициализация клавиатуры
+    private void addButtons(SendMessage sendMessage, Message message) {
+        var currentCommands = getUserCommands(message);
+        var replyKeyboardMarkup = getReplyKeyboardMarkup();//инициализация клавиатуры
         sendMessage.setReplyMarkup(replyKeyboardMarkup);
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);//не скрывать клаву
-
-        List<KeyboardRow> keyboardRowList = new ArrayList<KeyboardRow>();
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
         KeyboardRow keyboardFirstRow = new KeyboardRow();
-
         for (String command : currentCommands.keySet()) {
             keyboardFirstRow.add(new KeyboardButton(command));
         }
-
         keyboardRowList.add(keyboardFirstRow);
         replyKeyboardMarkup.setKeyboard(keyboardRowList);
     }
 
-    public void updateButtons(SendMessage sendMessage) {
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();//инициализация клавиатуры
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);//не скрывать клаву
-
-        List<KeyboardRow> keyboardRowList = new ArrayList<KeyboardRow>();
-        KeyboardRow keyboardFirstRow = new KeyboardRow();
-
-        for (String command : currentCommands.keySet()) {
-            keyboardFirstRow.add(new KeyboardButton(command));
-        }
-
-        keyboardRowList.add(keyboardFirstRow);
-        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+    private void printFile(String nameFile, Message message) {
+        sendMsg(message, reader.readFile(nameFile));
     }
 
-    public void printFile(String nameFile, Message message) {
-        try{
-            File file = new java.io.File(nameFile);
-            FileReader fileReader = new FileReader(file, StandardCharsets.UTF_8);
-            BufferedReader reader = new BufferedReader(fileReader);
-            String line = reader.readLine();
-            StringBuffer text = new StringBuffer();
-
-            while (line != null) {
-                text.append(line + "\n");
-                line = reader.readLine();
-            }
-            sendMsg(message, text.toString());
+    private void setCurrentParagraphsList(UserDates userDates) {
+        try {
+            var bookName = reader.getCurrentBookName(userDates.getCurrentBook());
+            userDates.setCurrentParagraphsList(googleDrive.getParagraphsList(googleDrive.getTextByGoogleDisk(googleDrive.getDrive(), bookName)));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public HashMap<String, MyFunc> createPrimitiveCommands() {
+    ///////////////////
+    private State getUserState(Message message){
+        String chatId = message.getChatId().toString();
+        if (Users.containsKey(chatId))
+            return Users.get(chatId).getState();
+        addNewUser(chatId);
+        return Users.get(chatId).getState();
+    }
+
+    private HashMap<String, MyFunc> getUserCommands(Message message){
+        String chatId = message.getChatId().toString();
+        if (Users.containsKey(chatId))
+            return Users.get(chatId).getCurrentCommands();
+        addNewUser(chatId);
+        return Users.get(chatId).getCurrentCommands();
+    }
+
+    private UserDates getUserDates(Message message){
+        String chatId = message.getChatId().toString();
+        if (Users.containsKey(chatId))
+            return Users.get(chatId);
+        addNewUser(chatId);
+        return Users.get(chatId);
+    }
+
+    private void addNewUser(String chatId){
+        var dates = new UserDates();
+        dates.setCurrentCommands(createPrimitiveCommands());
+        Users.put(chatId, dates);
+    }
+
+    //////////////////
+    private HashMap<String, MyFunc> createPrimitiveCommands() {
         HashMap<String, MyFunc> commands = new HashMap<>();
-        commands.put("/help", (message -> help(message)));
-        commands.put("/echo", (message -> echo(message)));
-        commands.put("/authors", (message -> authors(message)));
-        commands.put("/printDate", (message -> printDate(message)));
-        commands.put("/library", (message -> library(message)));
+        commands.put("/help", (this::help));
+        commands.put("/echo", (this::echo));
+        commands.put("/authors", (this::authors));
+        commands.put("/printDate", (this::printDate));
+        commands.put("/library", (this::library));
         return commands;
     }
 
-    public HashMap<String, MyFunc> createLibraryCommands() {
+    private HashMap<String, MyFunc> createLibraryCommands() {
         HashMap<String, MyFunc> commands = new HashMap<>();
-        commands.put("/help", (message -> help(message)));
-        commands.put("/exitToMain", (message -> exitToMain(message)));
+        commands.put("/help", (this::help));
+        commands.put("/exitToMain", (this::exitToMain));
         commands.put("chooseBook", (message -> {
             sendMsg(message, "Введите номер книги");
-            chooseBook(message);}));
+            chooseBook(message);
+        }));
         return commands;
     }
 
-    private HashMap<String,MyFunc> createReadCommands() {
+    private HashMap<String, MyFunc> createReadCommands() {
         HashMap<String, MyFunc> commands = new HashMap<>();
-        commands.put("/help", (message -> help(message)));
+        commands.put("/help", (this::help));
         // commands.put("/exitToMain", (message -> exitToMain(bot, botState, message)));
-        commands.put("/library", (message -> library(message)));
+        commands.put("/library", (this::library));
         commands.put("/infoAboutAuthor", (message -> {
             try {
                 getInfoAbAuthor(message);
@@ -197,139 +173,96 @@ public class Bot extends TelegramLongPollingBot {
                 e.printStackTrace();
             }
         }));
-        commands.put("ᐅ", (message -> {
-
-            readNext(message);
-
-        }));
+        commands.put("ᐅ", (this::readNext));
         //подумать над тем, как пользователь будет получать выбранный абзац
         return commands;
     }
 
-    public void help(Message message){
-        if(botState == State.Main)
+    //////////////////
+    private void help(Message message) {
+        var botState = getUserState(message);
+        if (botState.getCurrentState() == State.state.Main)
             printFile("src\\main\\resources\\helpMain.txt", message);
-        else if(botState == State.Library)
+        else if (botState.getCurrentState() == State.state.Library)
             printFile("src\\main\\resources\\helpLibrary.txt", message);
-        else if (botState == State.Read)
+        else if (botState.getCurrentState() == State.state.Read)
             printFile("src\\main\\resources\\helpRead.txt", message);
-
-
     } // сделать функции перехода. не сейчас
 
-    public void authors(Message message) {
+    private void authors(Message message) {
         printFile("src\\main\\resources\\authors.txt", message);
     }
 
-    public void echo(Message message) {
-        if (flEcho)
+    private void echo(Message message) {
+        var userDates = getUserDates(message);
+        if (userDates.getFlEcho())
             sendMsg(message, message.getText());
-        flEcho = true;
+        userDates.setFlEcho(true);
     }
 
-    public void printDate(Message message){
+    private void printDate(Message message) {
         Date date = new Date();
-        Locale local = new Locale("ru","RU");
-        DateFormat df = DateFormat.getDateTimeInstance (DateFormat.LONG, DateFormat.LONG, local);
+        Locale local = new Locale("ru", "RU");
+        DateFormat df = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, local);
         sendMsg(message, df.format(date));
     }
 
-    public void library(Message message){
-        botState = State.Library;
-        currentCommands = createLibraryCommands();
+    private void library(Message message) {
+        var userDates = getUserDates(message);
+        userDates.getState().setCurrentState(State.state.Library);
+        userDates.setCurrentCommands(createLibraryCommands());
         sendMsg(message, "Вы в библиотеке.");
         printFile("src\\main\\resources\\library.txt", message);
     }
 
-    public void exitToMain(Message message){
-        botState = State.Main;
-        currentCommands = createPrimitiveCommands();
+    private void exitToMain(Message message) {
+        var userDates = getUserDates(message);
+        userDates.getState().setCurrentState(State.state.Main);
+        userDates.setCurrentCommands(createPrimitiveCommands());
         sendMsg(message, "Вы вышли в главное меню.");
     }
 
-    public void chooseBook(Message message){
-        if (flChoose){
-            botState = State.Read;
-            currentCommands = createReadCommands();
+    private void chooseBook(Message message) {
+        var userDates = getUserDates(message);
+        var countBookInLibrary = reader.getCountLinesInFile("src\\main\\resources\\library.txt");
+        if (userDates.getFlChoose()) {
+            userDates.getState().setCurrentState(State.state.Read);
+            userDates.setCurrentCommands(createReadCommands());
             var number = Integer.parseInt(message.getText());
-            if(number < 0 || countBookInLibrary < number){
+            if (number < 0 || countBookInLibrary < number) {
                 sendMsg(message, "Неправильно выбран номер книги");
-                botState = State.Library;
-                currentCommands = createLibraryCommands();
+                userDates.getState().setCurrentState(State.state.Library);
+                userDates.setCurrentCommands(createLibraryCommands());
             } else {
                 sendMsg(message, "Приятного чтения");
-                currentBook = Integer.parseInt(message.getText());
-                setCurrentParagraphsList();
-                sendMsg(message, currentParagraphsList.get(currentPosition)); //выводит первый абзац
-                currentPosition++;
+                userDates.setCurrentBook(Integer.parseInt(message.getText()));
+                setCurrentParagraphsList(userDates);
             }
         }
-        flChoose = true;
+        userDates.setFlChoose(true);
     }
 
-    public void getInfoAbAuthor(Message message) throws Exception {
-        String site = readFileLine("src\\main\\resources\\library-authors-wiki-link.txt", currentBook);
+    private void getInfoAbAuthor(Message message) throws Exception {
+        var book = getUserDates(message).getCurrentBook();
+        String site = reader.readFileLine("src\\main\\resources\\library-authors-wiki-link.txt", book);
         String info = URLReader.GetInfo(site.substring(2), URLReader.InfoAbout.Author);
         sendMsg(message, info);
     }
 
-    public void getThumbnailSketch(Message message) throws Exception {
-        String site = readFileLine("src\\main\\resources\\library-wiki-link.txt", currentBook);
+    private void getThumbnailSketch(Message message) throws Exception {
+        var book = getUserDates(message).getCurrentBook();
+        String site = reader.readFileLine("src\\main\\resources\\library-wiki-link.txt", book);
         String info = URLReader.GetInfo(site.substring(2), URLReader.InfoAbout.ThumbnailSketchBook);
-        StringBuffer text = new StringBuffer();
-        var arInfo = info.toCharArray();
-        for (int i = 0; i < arInfo.length; i++){
-            if (arInfo[i] == '\n'){
-                sendMsg(message, text.toString());
-                text = new StringBuffer(); //затормозить выход
-            }
-            else
-                text.append(arInfo[i]);
+        String[] lines = info.split("\r\n|\r|\n");
+        for (String line : lines){
+            sendMsg(message, line);
         }
     }
 
-    private String readFileLine(String nameFile, int n){
-        try {
-            File file = new java.io.File(nameFile);
-            FileReader fileReader = new FileReader(file, StandardCharsets.UTF_8);
-            BufferedReader reader = new BufferedReader(fileReader);
-            int count = 1;
-            String line = reader.readLine();
-            while ((line != null) && (count < n)) {
-                line = reader.readLine();
-                count++;
-            }
-            reader.close();
-            return line;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "";
-        }
-    }
-
-    public void readNext(Message message){
-        sendMsg(message, currentParagraphsList.get(currentPosition));
-        currentPosition++;
-    }
-    
-    private String getCurrentBookName() {
-        var result = new StringBuffer();
-        var name = readFileLine("library.txt", currentBook).toCharArray();
-        var fl= false;
-        for(var i =0; i < name.length; i++){
-            if (fl)
-                result.append(name[i]);
-            if(name[i] == '"')
-                fl = !fl;
-        }
-        return result.deleteCharAt(result.length() - 1).toString();
-    }
-
-    private void setCurrentParagraphsList(){
-        try {
-            currentParagraphsList = googleDrive.getParagraphsList(googleDrive.getTextByGoogleDisk(googleDrive.getDrive(), getCurrentBookName()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void readNext(Message message) {
+        var userDates = getUserDates(message);
+        var pos = userDates.getCurrentPosition();
+        sendMsg(message, userDates.getCurrentParagraphsList().get(pos));
+        userDates.setCurrentPosition(pos + 1);
     }
 }
