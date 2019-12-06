82a65b1e0304f1fd221a9ec12657206c23c9579c
ValeriaIM <50073123+ValeriaIM@users.noreply.github.com>
2019-12-06 23:51:54 +0500

    Внесены правки от IJ

--- Quiz.java
+++ Quiz.java
@@ -1,44 +1,44 @@
-public class Quiz {
+class Quiz {
     private String[] questions;
     private String[] answers;
     private String currentAnswer = "0";
     private int currentQuestion = 0;
     private int correctAnswers = 0;
 
-    public Quiz(String[] questions, String[] answers) {
+    Quiz(String[] questions, String[] answers) {
         this.questions = questions;
         this.answers = answers;
     }
 
-    public String[] getQuestions() {
+    String[] getQuestions() {
         return questions;
     }
 
-    public String[] getAnswers() {
+    String[] getAnswers() {
         return answers;
     }
-    
-    public String getCurrentAnswer() {
+
+    String getCurrentAnswer() {
         return currentAnswer;
     }
-    
-    public int getCurrentQuestion() {
+
+    int getCurrentQuestion() {
         return currentQuestion;
     }
 
-    public int getCorrectAnswers() {
+    int getCorrectAnswers() {
         return correctAnswers;
     } //getters
 
-    public void setCurrentAnswer(String currentAnswer) {
+    void setCurrentAnswer(String currentAnswer) {
         this.currentAnswer = currentAnswer;
     }
-    
-    public void nextQuestion() {
+
+    void nextQuestion() {
         currentQuestion++;
     }
 
-    public void addCorrectAnswers() {
+    void addCorrectAnswers() {
         correctAnswers++;
     }
 }
