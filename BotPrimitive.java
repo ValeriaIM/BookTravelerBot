a444a113a75784252a1efc2f1045b11f43559bce
ValeriaIM <50073123+ValeriaIM@users.noreply.github.com>
2019-12-06 22:30:35 +0500

    Поправила стиль

--- BotPrimitive.java
+++ BotPrimitive.java
@@ -4,9 +4,11 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 
+import java.io.IOException;
+
 public class BotPrimitive extends TelegramLongPollingBot {
-    interface MyFunc{
-        void func(Message message);
+    interface MyFunc {
+        void func(Message message) throws IOException;
     }
 
     @Override
@@ -24,7 +26,7 @@ public class BotPrimitive extends TelegramLongPollingBot {
         return "741739778:AAFfKTQkbLQkePnWPJaRhe11uAJFnUYcfaM";
     }
 
-    public static ReplyKeyboardMarkup getReplyKeyboardMarkup(){
+    public static ReplyKeyboardMarkup getReplyKeyboardMarkup() {
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();//инициализация клавиатуры
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
@@ -32,7 +34,7 @@ public class BotPrimitive extends TelegramLongPollingBot {
         return replyKeyboardMarkup;
     }
 
-    public static SendMessage getSendMessage(Message message, String text){
+    public static SendMessage getSendMessage(Message message, String text) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
