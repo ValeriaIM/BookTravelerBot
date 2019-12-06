db97b0ff6a6a81f752b18433f2ac33973c57fba2
ValeriaIM <50073123+ValeriaIM@users.noreply.github.com>
2019-12-06 23:45:54 +0500

    Внесены правки от IJ

--- BotCommands.java
+++ BotCommands.java
@@ -5,8 +5,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class BotCommands {
-    public String help(Message message, BotLogic botLogic, Bot bot) {
+class BotCommands {
+    String help(Message message, BotLogic botLogic, Bot bot) {
         String chatId = message.getChatId().toString();
         var botState = botLogic.getUserState(chatId, bot);
         if (botState.getCurrentState() == State.state.Main)
@@ -20,39 +20,37 @@ public class BotCommands {
         return "";
     } // сделать функции перехода. не сейчас
 
-    public String getDate() {
+    String getDate() {
         Date date = new Date();
         Locale local = new Locale("ru", "RU");
         DateFormat df = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, local);
         return df.format(date);
     }
 
-    public String library(Message message, BotLogic botLogic, Bot bot) {
+    String library(Message message, BotLogic botLogic, Bot bot) {
         var userDates = botLogic.getUserData(message.getChatId().toString(), bot);
         if (userDates.getState().getCurrentState() == State.state.Quiz) {
             var quantity = userDates.getCurrentQuiz().getAnswers().length;
-            var text = "Викторина завершена. Количество правильных ответов: " + userDates.getCurrentQuiz().getCorrectAnswers() + "/" + quantity;
-            return text;
+            return "Викторина завершена. Количество правильных ответов: " + userDates.getCurrentQuiz().getCorrectAnswers() + "/" + quantity;
         }
         userDates.getState().setCurrentState(State.state.Library);
         return "";
     }
 
-    public String getInfoAbAuthor(String chatId, BotLogic botLogic, Bot bot) throws Exception {
+    String getInfoAbAuthor(String chatId, BotLogic botLogic, Bot bot) throws Exception {
         var book = botLogic.getUserData(chatId, bot).getCurrentBook();
         String site = botLogic.getReader().readFileLine("src\\main\\resources\\library-authors-wiki-link.txt", book);
-        String info = URLReader.GetInfo(site.substring(2), URLReader.InfoAbout.Author);
-        return info;
+        return URLReader.GetInfo(site.substring(2), URLReader.InfoAbout.Author);
     }
 
-    public String[] getThumbnailSketch(String chatId, BotLogic botLogic, Bot bot) throws Exception {
+    String[] getThumbnailSketch(String chatId, BotLogic botLogic, Bot bot) throws Exception {
         var book = botLogic.getUserData(chatId, bot).getCurrentBook();
         String site = botLogic.getReader().readFileLine("src\\main\\resources\\library-wiki-link.txt", book);
         String info = URLReader.GetInfo(site.substring(2), URLReader.InfoAbout.ThumbnailSketchBook);
         return info.split("\r\n|\r|\n");
     }
 
-    public String handleFirstEntry(UserData userData, BotLogic botLogic, String chatId) throws IOException {
+    String handleFirstEntry(UserData userData, BotLogic botLogic, String chatId) throws IOException {
         userData.getState().setCurrentState(State.state.Quiz);
         botLogic.createQuiz(chatId);
         var question = userData.getCurrentQuiz().getQuestions()[0];
@@ -60,15 +58,15 @@ public class BotCommands {
         return "1/" + quantity + ". " + question;
     }
 
-    public String getNextQuestion(UserData userData, int quantity) {
+    String getNextQuestion(UserData userData, int quantity) {
         var n = userData.getCurrentQuiz().getCurrentQuestion();
         var question = userData.getCurrentQuiz().getQuestions()[n].substring(2);
         n++;
         return n + "/" + quantity + ". " + question;
     }
 
-    public String chooseBook(Message message, BotLogic botLogic, UserData userData) {
-        var countBookInLibrary = botLogic.getReader().getCountLinesInFile("src\\main\\resources\\library.txt");
+    String chooseBook(Message message, BotLogic botLogic, UserData userData) {
+        var countBookInLibrary = botLogic.getReader().getCountLinesInFile();
         if (userData.getFlChoose()) {
             userData.getState().setCurrentState(State.state.Read);
             var number = 0;
