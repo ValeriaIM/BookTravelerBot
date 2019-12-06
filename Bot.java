67814323564fbb5548ade103690fe53809adc740
ValeriaIM <50073123+ValeriaIM@users.noreply.github.com>
2019-12-06 22:27:59 +0500

    Сокращенная версия, вынесла логику и части команд

--- Bot.java
+++ Bot.java
@@ -1,4 +1,3 @@
-import org.o7planning.googledrive.quickstart.GoogleDrive;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -9,23 +8,14 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-import java.io.*;
-import java.security.GeneralSecurityException;
-import java.text.DateFormat;
-import java.util.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Bot extends BotPrimitive {
-    private HashMap<String, UserDates> Users = new HashMap<>();
-    private Reader reader = new Reader();
-    private GoogleDrive googleDrive;
-
-    {
-        try {
-            googleDrive = new GoogleDrive();
-        } catch (IOException | GeneralSecurityException e) {
-            e.printStackTrace();
-        }
-    }
+    private BotLogic botLogic = new BotLogic();
+    private BotCommands botCommands = new BotCommands();
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
@@ -51,11 +41,11 @@ public class Bot extends BotPrimitive {
 
     private void processingMessage(Message message) throws IOException {
         var chatId = message.getChatId().toString();
-        if (!Users.containsKey(chatId)) {
-            addNewUser(chatId);
+        if (!botLogic.getUsers().containsKey(chatId)) {
+            botLogic.getUsers().put(chatId, createUser(chatId));
         }
-        var currentCommands = getUserCommands(message);
-        var userDates = getUserDates(message);
+        var currentCommands = botLogic.getUserCommands(chatId, this);
+        var userDates = botLogic.getUserData(chatId, this);
         if (userDates.getFlEcho()) {
             echo(message);
             userDates.setFlEcho(false);
@@ -80,7 +70,7 @@ public class Bot extends BotPrimitive {
     }
 
     private void addButtons(SendMessage sendMessage, Message message) {
-        var currentCommands = getUserCommands(message);
+        var currentCommands = botLogic.getUserCommands(message.getChatId().toString(), this);
         var replyKeyboardMarkup = getReplyKeyboardMarkup();//инициализация клавиатуры
         sendMessage.setReplyMarkup(replyKeyboardMarkup);
         List<KeyboardRow> keyboardRowList = new ArrayList<>();
@@ -89,62 +79,27 @@ public class Bot extends BotPrimitive {
         for (String command : currentCommands.keySet()) {
             keyboardFirstRow.add(new KeyboardButton(command));
             count++;
-            if (count == 3){
+            if (count == 3) {
                 keyboardRowList.add(keyboardFirstRow);
                 keyboardFirstRow = new KeyboardRow();
                 count = 0;
             }
         }
-        if (keyboardFirstRow != null)
-            keyboardRowList.add(keyboardFirstRow);
+        keyboardRowList.add(keyboardFirstRow);
         replyKeyboardMarkup.setKeyboard(keyboardRowList);
     }
 
     private void printFile(String nameFile, Message message) {
-        sendMsg(message, reader.readFile(nameFile));
+        sendMsg(message, botLogic.getReader().readFile(nameFile));
     }
 
-    private void setCurrentParagraphsList(UserDates userDates) {
-        try {
-            var bookName = reader.getCurrentBookName(userDates.getCurrentBook());
-            userDates.setCurrentParagraphsList(googleDrive.getParagraphsList(googleDrive.getTextByGoogleDisk(googleDrive.getDrive(), bookName)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public UserData createUser(String chatId) {
+        var data = new UserData();
+        data.setCurrentCommands(createPrimitiveCommands());
+        return data;
     }
 
-    ///////////////////
-    private State getUserState(Message message){
-        String chatId = message.getChatId().toString();
-        if (Users.containsKey(chatId))
-            return Users.get(chatId).getState();
-        addNewUser(chatId);
-        return Users.get(chatId).getState();
-    }
-
-    private HashMap<String, MyFunc> getUserCommands(Message message){
-        String chatId = message.getChatId().toString();
-        if (Users.containsKey(chatId))
-            return Users.get(chatId).getCurrentCommands();
-        addNewUser(chatId);
-        return Users.get(chatId).getCurrentCommands();
-    }
-
-    private UserDates getUserDates(Message message){
-        String chatId = message.getChatId().toString();
-        if (Users.containsKey(chatId))
-            return Users.get(chatId);
-        addNewUser(chatId);
-        return Users.get(chatId);
-    }
-
-    private void addNewUser(String chatId){
-        var dates = new UserDates();
-        dates.setCurrentCommands(createPrimitiveCommands());
-        Users.put(chatId, dates);
-    }
-
-    //////////////////
+    ////////////////
     private HashMap<String, MyFunc> createPrimitiveCommands() {
         HashMap<String, MyFunc> commands = new HashMap<>();
         commands.put("?", (this::help));
@@ -197,20 +152,11 @@ public class Bot extends BotPrimitive {
         commands.put("2", (message -> checkAnswer(message, "2")));
         commands.put("3", (message -> checkAnswer(message, "3")));
         return commands;
-    }
+    } //createCommands
+    ///////////////
 
-
-    //////////////////
     private void help(Message message) {
-        var botState = getUserState(message);
-        if (botState.getCurrentState() == State.state.Main)
-            printFile("src\\main\\resources\\helpMain.txt", message);
-        else if (botState.getCurrentState() == State.state.Library)
-            printFile("src\\main\\resources\\helpLibrary.txt", message);
-        else if (botState.getCurrentState() == State.state.Read)
-            printFile("src\\main\\resources\\helpRead.txt", message);
-        else if (botState.getCurrentState() == State.state.Quiz)
-            printFile("src\\main\\resources\\helpQuiz.txt", message);
+        printFile(botCommands.help(message, botLogic, this), message);
     } // сделать функции перехода. не сейчас
 
     private void authors(Message message) {
@@ -218,120 +164,83 @@ public class Bot extends BotPrimitive {
     }
 
     private void echo(Message message) {
-        var userDates = getUserDates(message);
+        var userDates = botLogic.getUserData(message.getChatId().toString(), this);
         if (userDates.getFlEcho())
             sendMsg(message, message.getText());
         userDates.setFlEcho(true);
     }
 
     private void printDate(Message message) {
-        Date date = new Date();
-        Locale local = new Locale("ru", "RU");
-        DateFormat df = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, local);
-        sendMsg(message, df.format(date));
+        sendMsg(message, botCommands.getDate());
     }
 
     private void library(Message message) {
-        var userDates = getUserDates(message);
-        userDates.getState().setCurrentState(State.state.Library);
-        userDates.setCurrentCommands(createLibraryCommands());
+        sendMsg(message, botCommands.library(message, botLogic, this));
+        botLogic.getUserData(message.getChatId().toString(), this).setCurrentCommands(createLibraryCommands());
         sendMsg(message, "Вы в библиотеке.");
         printFile("src\\main\\resources\\library.txt", message);
     }
 
     private void checkAnswer(Message message, String answer) throws IOException {
-        var userDates = getUserDates(message);
+        var userDates = botLogic.getUserData(message.getChatId().toString(), this);
         userDates.getCurrentQuiz().setCurrentAnswer(answer);
         runQuiz(message);
     }
 
     private void runQuiz(Message message) throws IOException {
-        var userDates = getUserDates(message);
-        if (userDates.getState().getCurrentState() == State.state.Quiz) {
-            if(userDates.getCurrentQuiz().getCurrentQuestion() >= userDates.getCurrentQuiz().getAnswers().length){
-                sendMsg(message, "Викторина завершена.");
+        var userData = botLogic.getUserData(message.getChatId().toString(), this);
+        if (userData.getState().getCurrentState() == State.state.Quiz) {
+            var quantity = userData.getCurrentQuiz().getAnswers().length;
+            var n = userData.getCurrentQuiz().getCurrentQuestion();
+            var correctAnswer = userData.getCurrentQuiz().getAnswers()[n];
+            if (userData.getCurrentQuiz().getCurrentAnswer().equals(correctAnswer)) {
+                sendMsg(message, "Верно");
+                userData.getCurrentQuiz().addCorrectAnswers();
+            } else
+                sendMsg(message, "Неверно");
+            userData.getCurrentQuiz().nextQuestion();
+            if (userData.getCurrentQuiz().getCurrentQuestion() < userData.getCurrentQuiz().getAnswers().length)
+                sendMsg(message, botCommands.getNextQuestion(userData, quantity));
+            else
                 library(message);
-            } else {
-                var question = userDates.getCurrentQuiz().getCurrentQuestion();
-                var correctAnswer = userDates.getCurrentQuiz().getAnswers()[question];
-                if(userDates.getCurrentQuiz().getCurrentAnswer().equals(correctAnswer))
-                    sendMsg(message, "Верно");
-                else
-                    sendMsg(message, "Неверно");
-                userDates.getCurrentQuiz().nextQuestion();
-                if(userDates.getCurrentQuiz().getCurrentQuestion() < userDates.getCurrentQuiz().getAnswers().length)
-                    sendMsg(message, userDates.getCurrentQuiz().getQuestions()[userDates.getCurrentQuiz().getCurrentQuestion()]);
-                else {
-                    sendMsg(message, "Викторина завершена.");
-                    library(message);
-                }
-            }
-        } else{
-            userDates.getState().setCurrentState(State.state.Quiz);
-            createQuiz(message);
-            userDates.setCurrentCommands(createQuizCommands());
-            var question = userDates.getCurrentQuiz().getQuestions()[0];
-            sendMsg(message, question);
+        } else {
+            userData.setCurrentCommands(createQuizCommands());
+            sendMsg(message, botCommands.handleFirstEntry(userData, botLogic, message.getChatId().toString()));
         }
     }
 
     private void exitToMain(Message message) {
-        var userDates = getUserDates(message);
+        var userDates = botLogic.getUserData(message.getChatId().toString(), this);
         userDates.getState().setCurrentState(State.state.Main);
         userDates.setCurrentCommands(createPrimitiveCommands());
         sendMsg(message, "Вы вышли в главное меню.");
     }
 
     private void chooseBook(Message message) {
-        var userDates = getUserDates(message);
-        var countBookInLibrary = reader.getCountLinesInFile("src\\main\\resources\\library.txt");
-        if (userDates.getFlChoose()) {
-            userDates.getState().setCurrentState(State.state.Read);
-            userDates.setCurrentCommands(createReadCommands());
-            var number = Integer.parseInt(message.getText());
-            if (number < 0 || countBookInLibrary < number) {
-                sendMsg(message, "Неправильно выбран номер книги");
-                userDates.getState().setCurrentState(State.state.Library);
-                userDates.setCurrentCommands(createLibraryCommands());
-            } else {
-                sendMsg(message, "Приятного чтения");
-                userDates.setCurrentBook(Integer.parseInt(message.getText()));
-                setCurrentParagraphsList(userDates);
-            }
-        }
-        userDates.setFlChoose(true);
+        var userData = botLogic.getUserData(message.getChatId().toString(), this);
+        var text = botCommands.chooseBook(message, botLogic, userData);
+        if (text.equals("Неправильно выбран номер книги"))
+            userData.setCurrentCommands(createLibraryCommands());
+        else
+            userData.setCurrentCommands(createReadCommands());
+        sendMsg(message, text);
     }
 
     private void getInfoAbAuthor(Message message) throws Exception {
-        var book = getUserDates(message).getCurrentBook();
-        String site = reader.readFileLine("src\\main\\resources\\library-authors-wiki-link.txt", book);
-        String info = URLReader.GetInfo(site.substring(2), URLReader.InfoAbout.Author);
-        sendMsg(message, info);
+        sendMsg(message, botCommands.getInfoAbAuthor(message.getChatId().toString(), botLogic, this));
     }
 
     private void getThumbnailSketch(Message message) throws Exception {
-        var book = getUserDates(message).getCurrentBook();
-        String site = reader.readFileLine("src\\main\\resources\\library-wiki-link.txt", book);
-        String info = URLReader.GetInfo(site.substring(2), URLReader.InfoAbout.ThumbnailSketchBook);
-        String[] lines = info.split("\r\n|\r|\n");
-        for (String line : lines){
+        String[] lines = botCommands.getThumbnailSketch(message.getChatId().toString(), botLogic, this);
+        for (String line : lines) {
             sendMsg(message, line);
         }
     }
 
     private void readNext(Message message) {
-        var userDates = getUserDates(message);
+        var userDates = botLogic.getUserData(message.getChatId().toString(), this);
         var pos = userDates.getCurrentPosition();
         sendMsg(message, userDates.getCurrentParagraphsList().get(pos));
         userDates.setCurrentPosition(pos + 1);
-    }
-
-    private void createQuiz(Message message) throws IOException {
-        var userDates = getUserDates(message);
-        var name = reader.getCurrentBookName(userDates.getCurrentBook());
-        var answer = reader.readFile("src\\main\\resources\\quizs\\Answers\\" + name + ".Answers.txt").split(";\n");
-        //var arAnswers = googleDrive.getTextByGoogleDisk(googleDrive.getDrive(), name + ".Answers").split("\n");
-        var arQuestions = googleDrive.getTextByGoogleDisk(googleDrive.getDrive(), name + ".Questions").split(";");
-        userDates.setCurrentQuiz(new Quiz(arQuestions, answer));
-    }
+    } //commandsWithSendMessage
 }
