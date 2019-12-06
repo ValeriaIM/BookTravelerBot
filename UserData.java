63e10881fd5e7e584b384c63610a7c7461f6bc1d
ValeriaIM <50073123+ValeriaIM@users.noreply.github.com>
2019-12-06 23:55:27 +0500

    Внесены правки от IJ

--- UserData.java
+++ UserData.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class UserData {
+class UserData {
     private State state = new State();
     private HashMap<String, BotPrimitive.MyFunc> currentCommands;
     private Boolean flChoose = false;
@@ -11,62 +11,62 @@ public class UserData {
     private int currentPosition = 0;
     private Quiz currentQuiz;
 
-    public State getState() {
+    State getState() {
         return state;
     }
 
-    public HashMap<String, BotPrimitive.MyFunc> getCurrentCommands() {
+    HashMap<String, BotPrimitive.MyFunc> getCurrentCommands() {
         return currentCommands;
     }
-    
-    public Boolean getFlChoose() {
+
+    Boolean getFlChoose() {
         return flChoose;
     }
 
-    public Boolean getFlEcho() {
+    Boolean getFlEcho() {
         return flEcho;
     }
 
-    public int getCurrentBook() {
+    int getCurrentBook() {
         return currentBook;
     }
 
-    public ArrayList<String> getCurrentParagraphsList() {
+    ArrayList<String> getCurrentParagraphsList() {
         return currentParagraphsList;
     }
 
-    public int getCurrentPosition() {
+    int getCurrentPosition() {
         return currentPosition;
     }
-    
-    public Quiz getCurrentQuiz() {
+
+    Quiz getCurrentQuiz() {
         return currentQuiz;
     } //getters
-    
-    public void setCurrentCommands(HashMap<String, BotPrimitive.MyFunc> currentCommands) {
+
+    void setCurrentCommands(HashMap<String, BotPrimitive.MyFunc> currentCommands) {
         this.currentCommands = currentCommands;
     }
-    public void setFlChoose(Boolean flChoose) {
+    void setFlChoose(Boolean flChoose) {
         this.flChoose = flChoose;
     }
 
-    public void setFlEcho(Boolean flEcho) {
+    void setFlEcho(Boolean flEcho) {
         this.flEcho = flEcho;
     }
 
-    public void setCurrentBook(int currentBook) {
+    void setCurrentBook(int currentBook) {
         this.currentBook = currentBook;
     }
 
-    public void setCurrentParagraphsList(ArrayList<String> currentParagraphsList) {
+    void setCurrentParagraphsList(ArrayList<String> currentParagraphsList) {
         this.currentParagraphsList = currentParagraphsList;
     }
 
-    public void setCurrentPosition(int currentPosition) {
+    void setCurrentPosition(int currentPosition) {
         this.currentPosition = currentPosition;
     }
 
-    public void setCurrentQuiz(Quiz quiz) {
+    void setCurrentQuiz(Quiz quiz) {
         this.currentQuiz = quiz;
     } //setters
 }
