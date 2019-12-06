5236995c0407af358ad6b69c16d1d2375530719c
ValeriaIM <50073123+ValeriaIM@users.noreply.github.com>
2019-12-06 22:33:20 +0500

    стиль

--- URLReader.java
+++ URLReader.java
@@ -1,21 +1,21 @@
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 
 public class URLReader {
-    enum InfoAbout{
+    enum InfoAbout {
         Author,
         ThumbnailSketchBook,
-        Books
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         //System.out.println(GetInfo("https://ru.wikipedia.org/wiki/Java", InfoAbout.Author));
-        //System.out.println(GacetInfo("https://ru.wikipedia.org/wiki/Виноваты_звёзды_(роман)", InfoAbout.ThumbnailSketchBook));
+        //System.out.println(GetInfo("https://ru.wikipedia.org/wiki/Виноваты_звёзды_(роман)", InfoAbout.ThumbnailSketchBook));
         //System.out.println(GetThumbnailSketch("https://litlife.club/books/174926/read?page=7", "</div>", "<script src", "</script>", "<a id="));
     }
 
-    public static String GetThumbnailSketch(String site, String preBegin, String begin, String preEnd, String end) throws Exception{
+    public static String GetThumbnailSketch(String site, String preBegin, String begin, String preEnd, String end) throws Exception {
         URL website = new URL(site);
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(website.openStream()));
@@ -35,13 +35,12 @@ public class URLReader {
             if (fl) {
                 text.append(inputLine);
             }
-            if (flBegin && inputLine.contains(begin)){
+            if (flBegin && inputLine.contains(begin)) {
                 text.append(inputLine);
                 fl = true;
                 flBegin = false;
                 continue;
-            }
-            else
+            } else
                 flBegin = false;
             if (inputLine.contains(preBegin))
                 flBegin = true;
@@ -50,7 +49,7 @@ public class URLReader {
         return text.toString();
     } // необработанный абзац с javascript конструкцией
 
-    public static String ProcessText(String text){
+    public static String ProcessText(String text) {
         if (text.length() == 0)
             return "";
         StringBuilder newText = new StringBuilder("");
@@ -80,7 +79,7 @@ public class URLReader {
                 continue;
             }
             if ((textArray[i] == '&') && (textArray[i + 1] == '#')) {
-                if (flJ){
+                if (flJ) {
                     i += 4;
                     flJ = false;
                     continue;
@@ -109,7 +108,7 @@ public class URLReader {
         return newText.toString();
     }
 
-    public static String GetInfo(String site, InfoAbout info)throws Exception{
+    public static String GetInfo(String site, InfoAbout info) throws Exception {
         if (site == null)
             site = "https://ru.wikipedia.org/wiki/Java";
         if (info == InfoAbout.ThumbnailSketchBook) {
