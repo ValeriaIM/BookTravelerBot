032049c5e106413c8ec6732da9729199a775a0df
ValeriaIM <50073123+ValeriaIM@users.noreply.github.com>
2019-12-06 22:59:59 +0500

    расставила по порядку методы

--- Quiz.java
+++ Quiz.java
@@ -1,8 +1,8 @@
 public class Quiz {
     private String[] questions;
     private String[] answers;
-    private int currentQuestion = 0;
     private String currentAnswer = "0";
+    private int currentQuestion = 0;
     private int correctAnswers = 0;
 
     public Quiz(String[] questions, String[] answers) {
@@ -17,28 +17,28 @@ public class Quiz {
     public String[] getAnswers() {
         return answers;
     }
+    
+    public String getCurrentAnswer() {
+        return currentAnswer;
+    }
+    
+    public int getCurrentQuestion() {
+        return currentQuestion;
+    }
 
+    public int getCorrectAnswers() {
+        return correctAnswers;
+    } //getters
+
+    public void setCurrentAnswer(String currentAnswer) {
+        this.currentAnswer = currentAnswer;
+    }
+    
     public void nextQuestion() {
         currentQuestion++;
     }
 
     public void addCorrectAnswers() {
         correctAnswers++;
-    }
-
-    public int getCurrentQuestion() {
-        return currentQuestion;
-    }
-
-    public String getCurrentAnswer() {
-        return currentAnswer;
-    }
-
-    public void setCurrentAnswer(String currentAnswer) {
-        this.currentAnswer = currentAnswer;
-    }
-
-    public int getCorrectAnswers() {
-        return correctAnswers;
     }
 }
